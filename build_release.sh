bf71cd6c7a299c5609590dfbc31e559680230fb6
Yun Yeo <sw.yunsuk@gmail.com>
2021-08-12 14:08:15 +0900

    bump optimizer version to 0.11.5

--- build_release.sh
+++ build_release.sh
@@ -2,4 +2,4 @@
 docker run --rm -v "$(pwd)":/code \
   --mount type=volume,source="$(basename "$(pwd)")_cache",target=/code/target \
   --mount type=volume,source=registry_cache,target=/usr/local/cargo/registry \
-  cosmwasm/workspace-optimizer:0.11.4
+  cosmwasm/workspace-optimizer:0.11.5
