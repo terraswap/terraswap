e2e984e4014eddf587094b52bd4e8a87f432f84f
Bryan RHEE <bryan@delightlabs.io>
2022-03-29 16:26:22 +0900

    feat: added pool sum logic

--- packages/terraswap/src/balancer.rs
+++ packages/terraswap/src/balancer.rs
@@ -1,7 +1,7 @@
 // Will be moved into terraswap_interface (ToBeFixed)
-use cosmwasm_std::StdResult;
+use cosmwasm_std::{Addr, DepsMut, StdResult, Uint128, Order};
 use cw_storage_plus::Map;
-use crate::asset::{Asset, PairInfo};
+use crate::asset::{Asset, AssetInfo, PairInfo};
 
 pub struct NewCalculatedBalacedAssets<'a> {
     pub new_virtual_pairs: Map<'a, &'a str, PairInfo>,
@@ -22,11 +22,42 @@ pub struct NewCalculatedBalacedAssets<'a> {
 /// 6-2. If exceed, make the # of stable asset as the unmatched asset
 /// 7. Calibrate the pair & unmatched asset info
 pub fn calculate_balanced_assets(
+    deps: DepsMut,
     virtual_pairs: Map<&str, PairInfo>,
     unmatched_assets: Map<&str, Asset>,
     is_provide: bool,
     asset: Asset,
     reserved_asset: Asset,
 ) -> StdResult<NewCalculatedBalacedAssets> {
+    // 1. Summize the pool size fo the stableleg
+    let mut whole_stableleg_size = Uint128::from(0u128);
+    for may_unit_pair in virtual_pairs.range(deps.storage, None, None, Order::Ascending) {
+        let unit_pair = may_unit_pair.expect("Wrong asset info is given").1;
 
+        for unit_asset in unit_pair.asset_infos {
+            // this "1" does not mean 1st asset, but asset info from enumerated array
+            match unit_asset {
+                AssetInfo::NativeToken{ denom } => match denom.as_str() {
+                    "uusd" => {
+                        let unit_pool_addr: Addr = deps.api.addr_validate(unit_pair.contract_addr.as_str())?;
+                        let uusd_amount = unit_asset.query_pool(&deps.querier, deps.api, unit_pool_addr)?;
+                        whole_stableleg_size += uusd_amount;
+                    },
+                    _ => (),
+                },
+                _ => (),
+            }
+        }
+    }
+
+    // 2. Check the type of the input asset
+    // match asset.info {
+    //     AssetInfo::NativeToken{ denom } => match denom.as_str() {
+    //         "uusd" | "ukrw" => whole_stableleg_size += asset.amount,
+    //         _ => (),
+    //     },
+    //     AssetInfo::Token{ contract_addr } => (),
+    // };
+
+    // return;
 }
