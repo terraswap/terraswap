e350dda5e54e835f721b4a2b0dba6ab127cc1f3b
Bryan RHEE <bryan@delightlabs.io>
2022-04-14 20:37:38 +0900

    fix: fixed the ratio calculation

--- packages/terraswap/src/balancer.rs
+++ packages/terraswap/src/balancer.rs
@@ -79,22 +79,22 @@ pub fn calculate_balanced_assets(
         // If the protocol has used the reserved UST + the provided currency is stableleg
         // -> pay back first
         let mut is_stableleg_provide = false;
-        if used_reserved_asset.amount > Uint128::from(0u128) {
-            match asset.info {
-                AssetInfo::NativeToken{ denom } => match denom.as_str() {
-                    // TODO: how to treat 'ukrw' + how to avoid 'uluna' & IBC tokens
-                    UUSD => {
-                        is_stableleg_provide = true;
+        match asset.info {
+            AssetInfo::NativeToken{ denom } => match denom.as_str() {
+                // TODO: how to treat 'ukrw' + how to avoid 'uluna' & IBC tokens
+                UUSD => {
+                    is_stableleg_provide = true;
+                    if used_reserved_asset.amount > Uint128::zero() {
                         (temp_input_asset, res) = reserve_asset_process(temp_input_asset, res);
-                    },
-                    _ => (),
+                    }
                 },
                 _ => (),
-            };
-        }
+            },
+            _ => (),
+        };
 
         // If paid back complete, nothing to do. Done it.
-        if temp_input_asset.amount == Uint128::from(0u128) { return Ok(res); }
+        if temp_input_asset.amount == Uint128::zero() { return Ok(res); }
 
         if (is_stableleg_provide && res.new_unmatched_assets.contains_key(&String::from(UUSD))) || 
             (!is_stableleg_provide && !res.new_unmatched_assets.contains_key(&String::from(UUSD))) {
@@ -138,9 +138,12 @@ fn put_unmatched_asset(
 ) -> NewCalculatedBalacedAssets {
     for unit_asset in input_assets.iter() {
         let token_name = get_asset_name(unit_asset.clone());
+        let mut zero_asset = unit_asset.clone();
+        zero_asset.amount = Uint128::zero();
+
         let point = balanced_assets_info.new_unmatched_assets
             .entry(token_name)
-            .or_insert(unit_asset.clone());
+            .or_insert(zero_asset);
 
         point.amount += unit_asset.amount;
     }
@@ -157,10 +160,7 @@ fn try_pairing_with_unmatched_assets(
         balanced_assets_info.new_unmatched_assets.clone(),
     ).unwrap();
     
-    let mut provided_ust = &mut Asset {
-        info: AssetInfo::NativeToken{ denom: String::from("uusd") },
-        amount: Uint128::zero(),
-    };
+    let mut provided_ust = &mut _asset_generator(UUSD, true, 0, STABLELEG_DENOMINATOR);
 
     if input_assets.contains_key(&String::from(UUSD)) &&
         !balanced_assets_info.new_unmatched_assets.contains_key(&String::from(UUSD))
@@ -180,10 +180,7 @@ fn try_pairing_with_unmatched_assets(
         );
 
         if provided_ust.amount > Uint128::zero() {
-            let unit_ust = Asset {
-                info: AssetInfo::NativeToken{ denom: String::from("uusd") },
-                amount: Uint128::zero(),
-            };
+            let unit_ust = _asset_generator(UUSD, true, 0, STABLELEG_DENOMINATOR);
             let unmatched_usd_asset = balanced_assets_info.new_unmatched_assets.entry(String::from(UUSD)).or_insert(unit_ust);
             unmatched_usd_asset.amount += provided_ust.amount;
         }
@@ -217,7 +214,7 @@ fn try_pairing_with_unmatched_assets(
 
     if is_unmatched_riskleg {
         let before_reserve_ust = Asset {
-            info: AssetInfo::NativeToken{ denom: String::from("uusd") },
+            info: AssetInfo::NativeToken{ denom: String::from(UUSD) },
             amount: balanced_assets_info.new_reserved_asset.amount
                         .checked_mul(balanced_assets_info.reserve_usage_ratio).unwrap()
                         .checked_div( get_pow10(STABLELEG_DENOMINATOR) ).unwrap(),
@@ -238,6 +235,9 @@ fn try_pairing_with_unmatched_assets(
         balanced_assets_info.new_virtual_pairs = new_virtual_pairs;
     }
 
+    // Delete 0-amount entry
+    balanced_assets_info.new_unmatched_assets.retain(|_, v| v.amount != Uint128::zero() );
+
     balanced_assets_info
 }
 
@@ -247,12 +247,11 @@ fn actual_paring(
     mut pairset: HashMap<String, Pairset>,
     enumerated_assets: IterMut<String, Asset>
 ) -> (Asset, HashMap<String, Pairset>) {
-    let mut remain_ust = Asset {
-        info: AssetInfo::NativeToken { denom: String::from("uusd") },
-        amount: Uint128::from(0u128),
-    };
+    let mut remain_ust = _asset_generator(UUSD, true, 0, STABLELEG_DENOMINATOR);
 
     for (token_name, unmatched_unit_asset) in enumerated_assets {
+        if *token_name == String::from(UUSD) { continue; }
+
         let ust_portion = provided_ust.amount
                             .checked_mul(
                                 *portions.get(token_name)
@@ -302,7 +301,8 @@ fn calculate_weight_unmatched_assets(
         };
 
         let ratio = derive_unit_ratio(pairset);
-        let portion = unit_asset.amount.checked_mul(ratio).unwrap();
+        let portion = unit_asset.amount.checked_mul(ratio).unwrap()
+                                .checked_div(get_pow10(STABLELEG_DENOMINATOR)).unwrap();
         whole_portion += portion.clone();
 
         res.insert(token_name.clone(), ratio);
@@ -317,19 +317,36 @@ fn calculate_weight_unmatched_assets(
     Ok(res)
 }
 
+fn _asset_generator(symbol: &str, is_native: bool, amount: u128, denom: u32) -> Asset {
+    match is_native {
+        true => 
+            Asset {
+                info: AssetInfo::NativeToken { denom: String::from(symbol) },
+                amount: Uint128::from(amount)
+                            .checked_mul(get_pow10(denom)).unwrap(),
+            },
+        false => 
+            Asset {
+                info: AssetInfo::Token { contract_addr: String::from(symbol) },
+                amount: Uint128::from(amount)
+                            .checked_mul(get_pow10(denom)).unwrap(),
+            },
+    }
+}
+
 fn derive_unit_ratio(
     unit_pair: &Pairset
 ) -> Uint128 {
     // Riskleg amount * 10^6 / stableleg
     // 10^6 is already multiplied
     if unit_pair.riskleg_denominator < STABLELEG_DENOMINATOR {
-        unit_pair.riskleg.amount
+        unit_pair.stableleg.amount
             .checked_mul( get_pow10(2 * STABLELEG_DENOMINATOR - unit_pair.riskleg_denominator) ).unwrap()
-            .checked_div(unit_pair.stableleg.amount).unwrap()
+            .checked_div(unit_pair.riskleg.amount).unwrap()
     } else {
-        unit_pair.riskleg.amount
+        unit_pair.stableleg.amount
             .checked_mul( get_pow10(unit_pair.riskleg_denominator) ).unwrap()
-            .checked_div(unit_pair.stableleg.amount).unwrap()
+            .checked_div(unit_pair.riskleg.amount).unwrap()
     }
 }
 
@@ -400,6 +417,9 @@ mod test {
 
     #[test]
     fn test001_stable_provide_unmatched_stable() {
+        // Provide stabeleg
+        // Stable asset exists in the unmatched assets
+
         let mut before_state = initilaizer();
 
         let incoming_provide = _asset_generator(UUSD, true, 100, STABLELEG_DENOMINATOR);
@@ -421,27 +441,50 @@ mod test {
         ).unwrap();
 
         let mut expected_state = initilaizer();
-        expected_state.new_unmatched_assets.insert(String::from(UUSD), _asset_generator(UUSD, true, 100, STABLELEG_DENOMINATOR));
+        expected_state.new_unmatched_assets.insert(String::from(UUSD), _asset_generator(UUSD, true, 200, STABLELEG_DENOMINATOR));
 
         _state_print(&after_state, &expected_state);
         assert_eq!(after_state, expected_state);
     }
 
-    fn _asset_generator(symbol: &str, is_native: bool, amount: u128, denom: u32) -> Asset {
-        match is_native {
-            true => 
-                Asset {
-                    info: AssetInfo::NativeToken { denom: String::from(symbol) },
-                    amount: Uint128::from(amount)
-                                .checked_mul(get_pow10(denom)).unwrap(),
-                },
-            false => 
-                Asset {
-                    info: AssetInfo::Token { contract_addr: String::from(symbol) },
-                    amount: Uint128::from(amount)
-                                .checked_mul(get_pow10(denom)).unwrap(),
-                },
-        }
+    #[test]
+    fn test002_stable_provide_unmatched_risk_1() {
+        // Provide stabeleg
+        // Risk asset exists in the unmatched assets
+        // Provided asset > Unmatched assets
+
+        let mut before_state = initilaizer();
+
+        let incoming_provide = _asset_generator(UUSD, true, 1000, STABLELEG_DENOMINATOR);
+        let unmatched_asset = HashMap::from([
+            (String::from(LUNA), _asset_generator(LUNA, true, 1, STABLELEG_DENOMINATOR))
+        ]);
+        let reserved_ust = _asset_generator(UUSD, true, 100000, STABLELEG_DENOMINATOR);
+        before_state.new_unmatched_assets = unmatched_asset;
+        before_state.new_reserved_asset = reserved_ust;
+
+        let after_state = calculate_balanced_assets(
+            true,
+            incoming_provide,
+            before_state.new_virtual_pairs,
+            before_state.new_unmatched_assets,
+            before_state.new_reserved_asset,
+            before_state.new_used_reserved_asset,
+            before_state.reserve_usage_ratio,
+        ).unwrap();
+
+        let mut expected_state = initilaizer();
+        let luna_pair = expected_state.new_virtual_pairs.get_mut(&String::from(LUNA)).unwrap();
+        *luna_pair = Pairset{
+            riskleg: _asset_generator(LUNA, true, 101, STABLELEG_DENOMINATOR),
+            riskleg_denominator: 6,
+            stableleg: _asset_generator(UUSD, true, 10100, STABLELEG_DENOMINATOR),
+        };
+
+        expected_state.new_unmatched_assets.insert(String::from(UUSD), _asset_generator(UUSD, true, 900, STABLELEG_DENOMINATOR));
+
+        _state_print(&after_state, &expected_state);
+        assert_eq!(after_state, expected_state);
     }
 
     fn _state_print(
