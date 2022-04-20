8abce627595156fc274e00961c248a8fd0eb6083
Bryan RHEE <bryan@delightlabs.io>
2022-04-20 14:03:21 +0900

    test: added test #010

--- packages/terraswap/src/balancer.rs
+++ packages/terraswap/src/balancer.rs
@@ -973,6 +973,44 @@ mod test {
         assert_eq!(after_state, expected_state);
     }
 
+    #[test]
+    fn test010_risk_provide_risk_unmatched_asset_big_reserve() {
+        // Provide riskleg
+        // Riskleg unmatched asset
+        // big reserve UST
+
+        let mut before_state = initilaizer();
+
+        let incoming_provide = _asset_generator(LUNA, true, 1, STABLELEG_DENOMINATOR);
+        let unmatched_asset = HashMap::from([
+            (String::from(LUNA), _asset_generator(LUNA, true, 9, STABLELEG_DENOMINATOR)),
+        ]);
+        let reserved_ust = _asset_generator(UUSD, true, 100000, STABLELEG_DENOMINATOR);
+
+        before_state.new_unmatched_assets = unmatched_asset;
+        before_state.new_reserved_asset = reserved_ust;
+
+        let after_state = calculate_balanced_assets(
+            true,
+            incoming_provide,
+            before_state.new_virtual_pairs,
+            before_state.new_unmatched_assets,
+            before_state.new_reserved_asset,
+            before_state.new_used_reserved_asset,
+            before_state.reserve_usage_ratio,
+        ).unwrap();
+
+        let mut expected_state = initilaizer();
+        expected_state.new_unmatched_assets = HashMap::from([
+            (String::from(LUNA), _asset_generator(LUNA, true, 10, STABLELEG_DENOMINATOR)),
+        ]);
+
+        expected_state.new_reserved_asset = _asset_generator_raw(UUSD, true, Uint128::from(100000_000000u128));
+
+        _state_print(&after_state, &expected_state);
+        assert_eq!(after_state, expected_state);
+    }
+
     fn _state_print(
         after_state: &NewCalculatedBalacedAssets,
         expected_state: &NewCalculatedBalacedAssets) {
