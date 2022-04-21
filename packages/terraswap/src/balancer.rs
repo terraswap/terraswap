37164675b923f74357c9d05b985e0bafa49b7de6
Bryan RHEE <bryan@delightlabs.io>
2022-04-21 17:37:57 +0900

    test: added test #017

--- packages/terraswap/src/balancer.rs
+++ packages/terraswap/src/balancer.rs
@@ -1305,6 +1305,51 @@ mod test {
         assert_eq!(after_state, expected_state);
     }
 
+    #[test]
+    fn test_balancer_017_risk_withdraw_unmatched_small_stable_asset() {
+        // Withdraw riskleg
+        // Stablelg in the unmatched asset
+
+        let mut before_state = initilaizer();
+
+        let outgoing_withdraw = _asset_generator(LUNA, true, 10, STABLELEG_DENOMINATOR);
+        let unmatched_asset = HashMap::from([
+            (String::from(UUSD), _asset_generator(UUSD, true, 100, STABLELEG_DENOMINATOR)),
+        ]);
+        let reserved_ust = _asset_generator(UUSD, true, 1_000_000, STABLELEG_DENOMINATOR);
+
+        before_state.new_unmatched_assets = unmatched_asset;
+        before_state.new_reserved_asset = reserved_ust;
+
+        let after_state = calculate_balanced_assets(
+            false,
+            outgoing_withdraw,
+            before_state.new_virtual_pairs,
+            before_state.new_unmatched_assets,
+            before_state.new_reserved_asset,
+            before_state.new_used_reserved_asset,
+            before_state.reserve_usage_ratio,
+        ).unwrap();
+
+        let mut expected_state = initilaizer();
+
+        let luna_pair = expected_state.new_virtual_pairs.get_mut(&String::from(LUNA)).unwrap();
+        *luna_pair = Pairset{
+            riskleg: _asset_generator_raw(LUNA, true, Uint128::from(90_000000u128)),
+            riskleg_denominator: 6,
+            stableleg: _asset_generator_raw(UUSD, true, Uint128::from(9000_000000u128)),
+        };
+
+        expected_state.new_unmatched_assets = HashMap::from([
+            (String::from(UUSD), _asset_generator(UUSD, true, 1100, STABLELEG_DENOMINATOR)),
+        ]);
+
+        expected_state.new_reserved_asset = _asset_generator(UUSD, true, 1_000_000, STABLELEG_DENOMINATOR);
+
+        _state_print(&after_state, &expected_state);
+        assert_eq!(after_state, expected_state);
+    }
+
     fn _state_print(
         after_state: &NewCalculatedBalacedAssets,
         expected_state: &NewCalculatedBalacedAssets) {
